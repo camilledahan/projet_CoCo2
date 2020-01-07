cb0ff1d971e2f07876f878392584485b5a0c2883
Paul Meallet <paul.meallet@mastertp>
2020-01-07 17:47:43 +0100

    Set ActiveMq communication - Probleme action denied

--- frontend/app/src/main/java/fr/cocoteam/co2co2/MainActivity.java
+++ frontend/app/src/main/java/fr/cocoteam/co2co2/MainActivity.java
@@ -1,11 +1,26 @@
 package fr.cocoteam.co2co2;
 
+
 import android.support.v4.app.Fragment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+
+/*import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+*/
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import java.net.URISyntaxException;
 
 import fr.cocoteam.co2co2.view.ContactFragment;
 import fr.cocoteam.co2co2.view.FindCarFragment;
@@ -14,6 +29,8 @@ import fr.cocoteam.co2co2.view.ProfilFragment;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
+    //private MQTT mqtt = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +38,34 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(this);
+
+        //connectMQTT();
+        String messageString = "Hello World from Java!";
+
+        System.out.println("== START PUBLISHER ==");
+
+
+       MqttClient client = null;
+        try {
+            client = new MqttClient("tcp://localhost:1883", MqttClient.generateClientId(), new MemoryPersistence());
+
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+
+            client.connect(connOpts);
+            MqttMessage message = new MqttMessage();
+            message.setPayload(messageString.getBytes());
+
+            client.publish("iot_data", message);
+
+            System.out.println("\tMessage '"+ messageString +"' to 'iot_data'");
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+
+        }
+
+        System.out.println("== END PUBLISHER ==");
     }
 
     private boolean loadFragment (Fragment fragment){
@@ -34,6 +79,22 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         }
         return false;
     }
+
+/*    private void connectMQTT(){
+
+        this.mqtt = new MQTT();
+        try {
+            mqtt.setHost("localhost:1883");
+        } catch(URISyntaxException e){
+                e.printStackTrace();
+                toast("Uri Exception");
+        }
+        FutureConnection connection = mqtt.futureConnection();
+        connection.connect();
+        String message = "test_message";
+        connection.publish("test/topic",message.getBytes(), QoS.AT_LEAST_ONCE, true);
+        //toast("Connected");
+    }*/
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -64,5 +125,11 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         }
 
         return  loadFragment(fragment);
+    }
+
+
+    private void toast(String message)
+    {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
