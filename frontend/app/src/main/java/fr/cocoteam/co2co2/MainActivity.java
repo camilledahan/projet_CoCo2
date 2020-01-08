5c5074eaa3cddc361c82b9783b506b9b2f260ddf
Paul Meallet <paul.meallet@mastertp>
2020-01-08 10:55:37 +0100

    connect to CloudMQTT and send message

--- frontend/app/src/main/java/fr/cocoteam/co2co2/MainActivity.java
+++ frontend/app/src/main/java/fr/cocoteam/co2co2/MainActivity.java
@@ -1,11 +1,15 @@
 package fr.cocoteam.co2co2;
 
 
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -14,14 +18,28 @@ import android.widget.Toast;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 */
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttClientPersistence;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttPersistable;
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
+import java.lang.reflect.Array;
 import java.net.URISyntaxException;
+import java.security.Permission;
+import java.util.ArrayList;
+import java.util.Enumeration;
 
+import fr.cocoteam.co2co2.util.MqttHelper;
 import fr.cocoteam.co2co2.view.ContactFragment;
 import fr.cocoteam.co2co2.view.FindCarFragment;
 import fr.cocoteam.co2co2.view.MapFragment;
@@ -29,7 +47,8 @@ import fr.cocoteam.co2co2.view.ProfilFragment;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-    //private MQTT mqtt = null;
+
+    MqttHelper mqttHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,34 +58,137 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(this);
 
+            String[] permissions = {
+                    android.Manifest.permission.INTERNET,
+                    android.Manifest.permission.ACCESS_NETWORK_STATE,
+                    android.Manifest.permission.ACCESS_WIFI_STATE,
+                    android.Manifest.permission.WAKE_LOCK
+            };
+
+            ActivityCompat.requestPermissions(this, permissions,0);
+
+
+
         //connectMQTT();
         String messageString = "Hello World from Java!";
 
         System.out.println("== START PUBLISHER ==");
 
+        startMqtt();
 
-       MqttClient client = null;
-        try {
-            client = new MqttClient("tcp://localhost:1883", MqttClient.generateClientId(), new MemoryPersistence());
+
+
+        /*try {
+            mqttAndroidClient = new MqttAndroidClient(this, serverUri, clientId, new MqttClientPersistence() {
+                @Override
+                public void open(String clientId, String serverURI) throws MqttPersistenceException {
+
+                }
+
+                @Override
+                public void close() throws MqttPersistenceException {
+
+                }
+
+                @Override
+                public void put(String key, MqttPersistable persistable) throws MqttPersistenceException {
+
+                }
+
+                @Override
+                public MqttPersistable get(String key) throws MqttPersistenceException {
+                    return null;
+                }
+
+                @Override
+                public void remove(String key) throws MqttPersistenceException {
+
+                }
+
+                @Override
+                public Enumeration keys() throws MqttPersistenceException {
+                    return null;
+                }
+
+                @Override
+                public void clear() throws MqttPersistenceException {
+
+                }
+
+                @Override
+                public boolean containsKey(String key) throws MqttPersistenceException {
+                    return false;
+                }
+            });
+
 
             MqttConnectOptions connOpts = new MqttConnectOptions();
+
+            connOpts.setUserName(username);
+            connOpts.setPassword(password.toCharArray());
+            connOpts.setKeepAliveInterval(30);
             connOpts.setCleanSession(true);
 
-            client.connect(connOpts);
-            MqttMessage message = new MqttMessage();
-            message.setPayload(messageString.getBytes());
+            mqttClient.connect(connOpts);
 
-            client.publish("iot_data", message);
+            /* MqttMessage message = new MqttMessage();
+            message.setPayload(messageString.getBytes());
+            mqttClient.publish(subscriptionTopic, message);
+*/
+            /*try {
+                mqttClient.subscribe(subscriptionTopic, 0, null, new IMqttActionListener() {
+                    @Override
+                    public void onSuccess(IMqttToken asyncActionToken) {
+                        Log.w("Mqtt","Subscribed!");
+                    }
+
+                    @Override
+                    public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                        Log.w("Mqtt", "Subscribed fail!");
+                    }
+                });
+
+            } catch (MqttException ex) {
+                System.err.println("Exceptionst subscribing");
+                ex.printStackTrace();
+            }
 
             System.out.println("\tMessage '"+ messageString +"' to 'iot_data'");
 
         } catch (MqttException e) {
             e.printStackTrace();
 
-        }
+        } */
 
         System.out.println("== END PUBLISHER ==");
     }
+
+    private void startMqtt(){
+        mqttHelper = new MqttHelper(getApplicationContext());
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                Log.w("Debug",mqttMessage.toString());
+                //dataReceived.setText(mqttMessage.toString());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+    }
+
 
     private boolean loadFragment (Fragment fragment){
         if (fragment != null){
