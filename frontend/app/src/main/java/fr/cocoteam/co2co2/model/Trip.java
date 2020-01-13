dffd75802a7c3036b1eed3bcafc63908e9351220
Paul Meallet <paul.meallet@mastertp>
2020-01-13 14:26:17 +0100

    Update models

--- frontend/app/src/main/java/fr/cocoteam/co2co2/model/Trip.java
+++ frontend/app/src/main/java/fr/cocoteam/co2co2/model/Trip.java
@@ -1,14 +1,32 @@
 package fr.cocoteam.co2co2.model;
 
-public class Trip {
+import io.realm.RealmModel;
+import io.realm.RealmObject;
+
+public class Trip extends RealmObject {
     private String depart;
     private String arrivee;
 
+    public Trip(){}
 
     public Trip(String depart, String arrivee) {
         this.depart = depart;
         this.arrivee = arrivee;
     }
 
+    public String getDepart() {
+        return depart;
+    }
 
+    public void setDepart(String depart) {
+        this.depart = depart;
+    }
+
+    public String getArrivee() {
+        return arrivee;
+    }
+
+    public void setArrivee(String arrivee) {
+        this.arrivee = arrivee;
+    }
 }
--- frontend/app/src/main/java/fr/cocoteam/co2co2/model/User.java
+++ frontend/app/src/main/java/fr/cocoteam/co2co2/model/User.java
@@ -1,14 +1,41 @@
 package fr.cocoteam.co2co2.model;
 
+import java.util.ArrayList;
 import java.util.List;
+import fr.cocoteam.co2co2.model.UserMatch;
 
-public class User extends AbstractUser {
+import io.realm.Realm;
+import io.realm.RealmList;
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
 
-    private List<UserMatch> userMatches;
+public class User extends RealmObject {
 
-    public User(String name, Trip trip, List<UserMatch> userMatches) {
+    @PrimaryKey
+    private int userId;
+    private String name;
+    private String surname;
+    private Trip trip;
+    private int age;
+    private boolean isDriver;
+    private int telephone;
+    private String description;
+    private RealmList<UserMatch> userMatches;
 
-        super(name, trip);
+    public User() {
+
+    }
+
+    public User(int userId, String name, String surname, Trip trip, int age, boolean isDriver, int telephone, String description, RealmList<UserMatch> userMatches) {
+        this.userId = userId;
+        this.name = name;
+        this.surname = surname;
+        this.trip = trip;
+        this.age = age;
+        this.isDriver = isDriver;
+        this.telephone = telephone;
+        this.description = description;
         this.userMatches = userMatches;
     }
 }
+
